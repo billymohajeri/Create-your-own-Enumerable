1f92607049785904df6a0207c6b3dc834907de39
Billy Mohajeri <billy.mohajeri@gmail.com>
2023-03-02 16:20:53 +0200

    Remove comments

--- class.rb
+++ class.rb
@@ -12,11 +12,11 @@ class MyList
 end
 
 list = MyList.new([1, 2, 3, 4])
-# Test #all?
-puts(list.all? { |e| e < 5 }) # true
-puts(list.all? { |e| e > 5 }) # false
-# Test #any?
-puts(list.any? { |e| e == 2 }) # true
-puts(list.any? { |e| e == 5 }) # false
-# Test #filter?
-p(list.filter(&:even?)) # [2, 4]
+
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+
+p(list.filter(&:even?))
